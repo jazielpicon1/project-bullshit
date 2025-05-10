e1576dc5bb5cd1139e65a0be4f5db8270cee411f
WumboTheOG <64537736+jazielpicon1@users.noreply.github.com>
2025-05-10 14:50:55 -0400

    Karate Man walking and punch animations fixec
    -Fixed karate man punching loop
    -Karate can now switch directions of his walk animation

--- Scripts/player_1.gd
+++ Scripts/player_1.gd
@@ -1,18 +1,52 @@
 extends CharacterBody2D
 
+var state_machine
 var max_speed = 100
 var last_direction := Vector2(1,0)
 
+
+func _ready():
+	state_machine = $AnimationTree.get("parameters/playback")
+func get_input():
+	var cuurent = state_machine.get_current_node()
+	velocity = Vector2.ZERO
+	if Input.is_action_just_pressed("Basic Attack punch"):
+		state_machine.travel("Karate Man animations_punching")
+		return
+	if Input.is_action_pressed("Walk Foward"):
+		velocity.x += 1
+		$AnimatedSprite2D.scale.x = -0.2
+	if Input.is_action_pressed("Walk Backwards"):
+		velocity.x -= 1
+		$AnimatedSprite2D.scale.x = 0.2
+	if Input.is_action_pressed("Jump"):
+		velocity.y += 1
+	if Input.is_action_pressed("Crouch"):
+		velocity.y -= 1
+	velocity.normalized() * max_speed
+	if velocity.length() == 0:
+		state_machine.travel("Karate Man animations_standing")
+	if velocity.length() > 0:
+		state_machine.travel("Karate Man animations_walking")
+	
 func _physics_process(delta):
+	get_input()
 	var direction = Input.get_vector("Walk Backwards","Walk Foward", "Jump", "Crouch")
 	velocity = direction * max_speed
-	move_and_slide()
+	
 	
 	if direction.length() > 0:
 		last_direction = direction
 		play_movement_animation(direction)
 	else:
 		play_idle_animation(last_direction)
+	move_and_slide()
+	
+	#if direction.length() > 0:
+	#	last_direction = direction
+	#	play_movement_animation(direction)
+	#else:
+	#	play_idle_animation(last_direction)
 		
 func play_movement_animation(direction):
 	if direction.x > 0:
@@ -27,6 +61,8 @@ func play_idle_animation(direction):
 		$AnimatedSprite2D.play("standing")
 
 #func _input(event):
+	#if event.is_action_pressed("Basic Attack punch"):
+	#	$AnimatedSprite2D.play("punching")
+	
 	#if event.is_action_pressed("Walk Foward"):
 	#	$AnimatedSprite2D.play("kennywalk")
-		
