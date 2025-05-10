0c60b47d8fc0f2b951c5d8f2f5e03c32a7474ba6
WumboTheOG <64537736+jazielpicon1@users.noreply.github.com>
2025-05-10 11:55:46 -0400

    Made karate man walk in multiple directions
    -Karate can walk in all directions
    -Karate man now has an idle animation and walking animation

--- Scripts/player_1.gd
+++ Scripts/player_1.gd
@@ -1,5 +1,32 @@
 extends CharacterBody2D
 
-func _input(event):
-	if event.is_action_pressed("Walk Foward"):
-		$AnimatedSprite2D.play("walking")
+var max_speed = 100
+var last_direction := Vector2(1,0)
+
+func _physics_process(delta):
+	var direction = Input.get_vector("Walk Backwards","Walk Foward", "Jump", "Crouch")
+	velocity = direction * max_speed
+	move_and_slide()
+	
+	if direction.length() > 0:
+		last_direction = direction
+		play_movement_animation(direction)
+	else:
+		play_idle_animation(last_direction)
+		
+func play_movement_animation(direction):
+	if direction.x > 0:
+		$AnimatedSprite2D.play("kennywalk")
+	elif direction.x < 0:
+		$AnimatedSprite2D.play("kennywalk")
+		
+func play_idle_animation(direction):
+	if direction.x > 0:
+		$AnimatedSprite2D.play("standing")
+	elif direction.x < 0:
+		$AnimatedSprite2D.play("standing")
+
+#func _input(event):
+	#if event.is_action_pressed("Walk Foward"):
+	#	$AnimatedSprite2D.play("kennywalk")
+		
