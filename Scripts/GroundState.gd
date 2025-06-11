33deb15b80e64296f776ec9fd3049e4ed4f9dfbc
WumboTheOG <64537736+jazielpicon1@users.noreply.github.com>
2025-06-10 20:44:33 -0400

    Update #3: Animation Transition Overhaul
    -Broke down karate man's movement and inputs into 3 states
    -Ground State: Can move forward and backwards, can use ground attacks, can jump
    -Air State: Cannot use ground attack moves, cannot move back and forth. can use air attack moves(in the near future), can enter the landing state
    -Landing state: Going to be used purely to transition back into ground state
    -Moved alot of code from player1.gd into the other scripts
    -Added state_machine.gd to manage the states
    -Added state.gd to catagorizes the new states scripts
    -Started from now, all animations and input will be handled in GroundState.gd, AirState.gd, and LandingState.gd

--- Scripts/GroundState.gd
+++ Scripts/GroundState.gd
@@ -1,3 +1,49 @@
 extends State
 
 class_name GroundState
+
+@export var jumpVelocity : float = -500.0
+@export var air_state : State
+
+func state_input(event : InputEvent):
+	#Handles the jump animation.
+	if(event.is_action_pressed("Jump")):
+		jump()
+	#Handles the attack animations on the ground.
+	if(event.is_action_pressed("Attack Button")):
+		get_input()
+			
+
+func jump():
+	character.velocity.y = jumpVelocity
+	next_state = air_state
+	playback.travel("Karate Man animations_jump-start")
+	
+	
+func stop_motion():
+	if character.attack_animation:
+		character.velocity.x = 0
+		
+
+#Gets input from the user and plays the according animation
+func get_input():
+	if Input.is_action_just_pressed("Light Punch"):
+		playback.travel("Karate Man animations_light_punch")
+		character.attack_animation = true
+		stop_motion()
+		return
+	if Input.is_action_just_pressed("Heavy Punch"):
+		playback.travel("Karate Man animations_heavy punch")
+		character.attack_animation = true
+		stop_motion()
+		return
+	if Input.is_action_just_pressed("Light Kick"):
+		playback.travel("Karate Man animations_light kick")
+		character.attack_animation = true
+		stop_motion()
+		return
+	if Input.is_action_just_pressed("Heavy Kick"):
+		playback.travel("Karate Man animations_heavy kick")
+		character.attack_animation = true
+		stop_motion()
+		return
--- Scripts/player_1.gd
+++ Scripts/player_1.gd
@@ -1,19 +1,9 @@
 class_name Player1
 extends CharacterBody2D
 
-#@onready var state_machine: StateMachine = $"StateMachine"
-#@onready var animation : AnimationPlayer = $AnimationPlayer
-#
-#func _ready(): state_machine.init()
-#
-#func _process(delta): state_machine.process_frame(delta)
-#
-#func _physics_process(delta): state_machine.process_physics(delta)
-#
-#func _input(event): state_machine.process_input(event)
 
-
-var state_machine
+@onready var animation_tree : AnimationTree = $AnimationTree
+@onready var state_machine: StateMachine = $StateMachine
 @export var maxSpeed : float = 200.0
 @export var jumpVelocity : float = -500.0
 
@@ -27,75 +17,41 @@ var was_in_air : bool = false
 
 #sets state machine with the animations from the animation tree
 func _ready():
-	state_machine = $AnimationTree.get("parameters/playback")
+	animation_tree.active = true
 
-#Gets input from the user and plays the according animation
-func get_input():
-	var current = state_machine.get_current_node()
-	
-	update_animation()
-	update_facing_direction()
-	if Input.is_action_just_pressed("Light Punch") && is_on_floor():
-		state_machine.travel("Karate Man animations_light_punch")
-		attack_animation = true
-		stop_motion()
-		return
-	if Input.is_action_just_pressed("Heavy Punch") && is_on_floor():
-		state_machine.travel("Karate Man animations_heavy punch")
-		attack_animation = true
-		stop_motion()
-		return
-	if Input.is_action_just_pressed("Light Kick") && is_on_floor():
-		state_machine.travel("Karate Man animations_light kick")
-		attack_animation = true
-		stop_motion()
-		return
-	if Input.is_action_just_pressed("Heavy Kick") && is_on_floor():
-		state_machine.travel("Karate Man animations_heavy kick")
-		attack_animation = true
-		stop_motion()
-		return
 	
 func _physics_process(delta):
 	#Adds gravity to the characters
-	animation_finished()
+	update_animation()
+	update_facing_direction()
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		was_in_air = true
-		
-	if was_in_air == true:
-		land()
-		
-	was_in_air = false
 	
 	#Handles the movement and direction of the character
 	direction = Input.get_vector("Walk Backwards","Walk Forward", "Crouch","Jump")
 	
 	#Handles horizontal movement 
 	if not attack_animation:
-		if direction:
+		if direction.x != 0 && state_machine.check_if_can_move():
 			velocity.x = direction.x * maxSpeed
 		else:
 			velocity.x = move_toward(velocity.x, 0, maxSpeed)
-	
-	#Handles the jump animation.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		was_in_air = true
-		jump()
 	
 	#Resumes movement after attack animation is finished
 	if Input.is_action_just_released("Attack Button"):
 		resume_motion()
 
-	get_input()
 	move_and_slide()
 
 func update_animation():
+	animation_tree.set("parameters/Walk/blend_position", direction.x)
 	if not animation_locked:
-		if direction.x != 0 && state_machine.get_current_node() != "Karate Man animations_jump-fall": 
-			state_machine.travel("Karate Man animations_walking")
+		if direction.x != 0: 
+			#animation_tree.travel("Karate Man animations_walking")
+			$AnimatedSprite2D.play("kennywalk")
 		else:
-			state_machine.travel("Karate Man animations_standing")
+			$AnimatedSprite2D.play("standing")
+			#animation_tree.travel("Karate Man animations_standing")
 		
 func update_facing_direction():
 	if direction.x > 0:
@@ -103,18 +59,7 @@ func update_facing_direction():
 	elif direction.x < 0:
 		$AnimatedSprite2D.scale.x = 0.2
 		
-func jump():
-	velocity.y = jumpVelocity
-	state_machine.travel("Karate Man animations_jump-start")
-	animation_locked = true
-	
-func land():
-	state_machine.travel("Karate Man animations_jump-fall-loop") 
-	animation_locked = true
-	
-func stop_motion():
-	if attack_animation:
-		velocity.x = 0
+
 
 func resume_motion():
 	attack_animation = false
@@ -123,10 +68,3 @@ func resume_motion():
 	else:
 		velocity.x = move_toward(velocity.x, 0, maxSpeed)
 	
-
-
-func animation_finished():
-	var tempCurrent = state_machine.get_current_node()
-	if tempCurrent == "Karate Man animations_jump-fall-loop" && is_on_floor():
-		animation_locked = false
-		was_in_air = false
