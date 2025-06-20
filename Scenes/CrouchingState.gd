d01f997b38b0edc3fb08a6d31dcafd6b0b9f2537
WumboTheOG <64537736+jazielpicon1@users.noreply.github.com>
2025-06-20 19:29:09 -0400

    Crouching Attacks Added
    -Added the crouching punch and kick animations to the crouching state script
    -Added crouch-loop animation to provide better transition from crouching attacks back to the crouching animation

--- Scenes/CrouchingState.gd
+++ Scenes/CrouchingState.gd
@@ -8,7 +8,27 @@ func state_input(event : InputEvent):
 	#Handles the jump animation.
 	if(event.is_action_released("Crouch")):
 		next_state = ground_state
+	if(event.is_action_pressed("Attack Button")):
+		get_input()
 
 func on_exit():
 	if(next_state == ground_state):
 		playback.travel("Walk")
+		
+func get_input():
+	if Input.is_action_just_pressed("Light Punch"):
+		playback.travel("Karate Man animations_crouch punch")
+		character.attack_animation = true
+		return
+	if Input.is_action_just_pressed("Heavy Punch"):
+		playback.travel("Karate Man animations_crouch punch")
+		character.attack_animation = true
+		return
+	if Input.is_action_just_pressed("Light Kick"):
+		playback.travel("Karate Man animations_crouch kick")
+		character.attack_animation = true
+		return
+	if Input.is_action_just_pressed("Heavy Kick"):
+		playback.travel("Karate Man animations_crouch kick")
+		character.attack_animation = true
+		return
