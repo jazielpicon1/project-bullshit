7f552a2c656b89673049fb2a5109413a18f95e91
WumboTheOG <64537736+jazielpicon1@users.noreply.github.com>
2025-07-13 20:03:01 -0400

    Made the sandbox and quit button functional
    -idk i got tired of main menu just sitting there not being used so I made some of the buttons work so I can start using it

--- Scripts/titlemenu.gd
+++ Scripts/titlemenu.gd
@@ -6,3 +6,10 @@ extends Control
 func _ready() -> void:
 	#mainbanner.visible = true
 	pass
+
+func _on_mm_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_mm_sandbox_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level.tscn")
